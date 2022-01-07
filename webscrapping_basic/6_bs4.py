3fe04558439f35359a51c3d86e05d151148cd0c0
SEON UI LEE <sunnyinsummer@ewhain.net>
2022-01-07 13:18:21 +0900

    2:18:46

--- webscrapping_basic/6_bs4.py
+++ webscrapping_basic/6_bs4.py
@@ -17,4 +17,13 @@ print(soup.find("a", attrs={"class": "Nbtn_upload"}))
 
 rank1 = soup.find("li", attrs={"class": "rank01"})
 print(rank1.a.get_text())
-print(rank1.next_sibling.next_sibling)
+rank2 = rank1.next_sibling.next_sibling
+rank3 = rank2.next_sibling.next_sibling
+print(rank1.parent)
+rank2 = rank1.find_next_sibling("li")  # 줄바꿈이 있어서 next_sibling을 두 번 쓰는 것
+
+# print(rank1.find_next_siblings("li"))
+
+
+webtoon = soup.find("a", text="독립일기")
+print(webtoon)
